cf8723d079fc297ad85556f8f709154164c94718
daniel-dorroh <daniel.dorroh@gmail.com>
2020-07-09 11:33:15 -0500

    Added stack implementation

--- js/src/stack/stack.js
+++ js/src/stack/stack.js
@@ -5,15 +5,29 @@ export class Stack {
     this.stack_ = [];
   }
 
-  peek() {
-
+  isEmpty() {
+    return !this.stack_.length;
   }
 
-  push() {
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    const topIndex = this.stack_.length - 1;
+    return this.stack_[topIndex];
+  }
 
+  push(item) {
+    if (item === undefined || item === null) {
+      throw `pushed item is ${item}`;
+    }
+    this.stack_.push(item);
   }
 
   pop() {
-
+    if (this.isEmpty()) {
+      throw 'no items on stack to pop';
+    }
+    return this.stack_.pop();
   }
 }
--- js/src/stack/stack.test.js
+++ js/src/stack/stack.test.js
@@ -1,5 +1,73 @@
 import { Stack } from './stack';
 
-test('should construct a stack', () => {
-  expect(new Stack()).toBeDefined();
+/**
+ * constructor tests
+ */
+test('constructor should construct a stack', () => {
+  const stack = new Stack();
+  expect(stack).toBeDefined();
+  expect(stack.stack_).toHaveLength(0);
+});
+
+/**
+ * push tests
+ */
+test.each([null, undefined])('push %s throws', (item) => {
+  expect(() => new Stack().push(item)).toThrow(`pushed item is ${item}`);
+});
+
+test('push item and item added to stack', () => {
+  const stack = new Stack();
+  const item = 40;
+  stack.push(item);
+  expect(stack.stack_).toHaveLength(1);
+  expect(stack.stack_[0]).toBe(item);
+});
+
+/**
+ * pop tests
+ */
+test('pop empty stack throws', () => {
+  expect(() => new Stack().pop()).toThrow('no items on stack to pop');
+});
+
+test('pop returns items in lifo order', () => {
+  const stack = new Stack();
+  const item1 = 25;
+  const item2 = 35;
+  stack.push(item1);
+  stack.push(item2);
+  expect(stack.pop()).toBe(item2);
+  expect(stack.pop()).toBe(item1);
+});
+
+/**
+ * peek tests
+ */
+test('peek empty stack returns undefined', () => {
+  expect(new Stack().peek()).toBeUndefined();
+});
+
+test('peek stack returns top item', () => {
+  const stack = new Stack();
+  const item1 = 25;
+  const item2 = 35;
+  stack.push(item1);
+  stack.push(item2);
+  expect(stack.peek()).toBe(item2);
+  stack.pop();
+  expect(stack.peek()).toBe(item1);
+});
+
+/**
+ * isEmpty tests
+ */
+test('isEmpty true for empty stack', () => {
+  expect(new Stack().isEmpty()).toBe(true);
+});
+
+test('isEmpty false for stack with items', () => {
+  const stack = new Stack();
+  stack.push(25);
+  expect(stack.isEmpty()).toBe(false);
 });
