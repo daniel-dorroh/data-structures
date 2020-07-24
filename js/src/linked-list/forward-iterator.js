7c1fa32a59c5bcbd052c14a9014b1ba9836b6f4d
daniel-dorroh <daniel.dorroh@gmail.com>
2020-07-23 22:21:10 -0500

    Fixed an issue in the forward iterator that made it fail to end when iterating across a list of one item

--- js/src/linked-list/forward-iterator.js
+++ js/src/linked-list/forward-iterator.js
@@ -1,13 +1,13 @@
 
 export class ForwardIterator {
 
-  constructor(list) {
+  constructor(list, initialCurrentElement = null) {
     if (typeof list.getFront !== 'function'
         || typeof list.get !== 'function') {
       throw 'list does not have get and/or getFront methods';
     }
     this.list_ = list;
-    this.currentElement_ = null;
+    this.currentElement_ = initialCurrentElement;
     this.isLastElement_ = false;
   }
 
@@ -19,8 +19,8 @@ export class ForwardIterator {
       this.currentElement_ = this.list_.getFront();
     } else {
       this.currentElement_ = this.list_.get(this.currentElement_.next);
-      this.isLastElement_ = this.currentElement_.next === null;
     }
+    this.isLastElement_ = this.currentElement_.next === null;
     return {
       value: this.currentElement_,
       done: false,
