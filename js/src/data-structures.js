46c38dd80ec950b3587e4de5b98af206257e4fcc
daniel-dorroh <daniel.dorroh@gmail.com>
2020-08-07 14:29:09 -0500

    Exported new structures

--- js/src/data-structures.js
+++ js/src/data-structures.js
@@ -2,4 +2,6 @@
 export { Stack } from './stack/stack';
 export { SingleList } from './linked-list/single-list';
 export { DoubleList } from './linked-list/double-list';
+export { SkipList } from './linked-list/skip-list';
 export { Queue } from './queue/queue';
+export { HashTable } from './hash-table';
