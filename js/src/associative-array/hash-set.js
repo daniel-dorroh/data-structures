6d2d8c07141d2db9475b8870814e737c0bdd4b8a
daniel-dorroh <daniel.dorroh@gmail.com>
2020-08-11 19:58:06 -0500

    Created HashSet and tests

---
+++ js/src/associative-array/hash-set.js
@@ -0,0 +1,46 @@
+import { HashTable } from './hash-table';
+
+export class HashSet {
+
+  constructor(createKeyFunction = null) {
+    if (createKeyFunction !== null && typeof createKeyFunction !== 'function') {
+      throw 'createKeyFunction is not a function';
+    }
+    this.createKey_ = createKeyFunction !== null
+        ? createKeyFunction
+        : (item) => item;
+    this.items_ = new HashTable();
+    this.size_ = 0;
+  }
+
+  size() {
+    return this.size_;
+  }
+
+  add(item) {
+    if (item === null || item === undefined) {
+      throw `item is ${item}`;
+    }
+    const key = this.createKey_(item);
+    if (this.get(key) === null) {
+      this.items_.add(key, item);
+      this.size_++;
+    }
+    return key;
+  }
+
+  get(key) {
+    if (key === null || key === undefined) {
+      return null;
+    }
+    return this.items_.get(key);
+  }
+
+  remove(key) {
+    if (this.get(key) !== null) {
+      this.items_.remove(key);
+      this.size_--;
+    }
+  }
+
+}
---
+++ js/src/associative-array/hash-set.test.js
@@ -0,0 +1,106 @@
+import { HashSet } from './hash-set';
+
+/**
+ * constructor tests
+ */
+describe('constructor', () => {
+
+  const variousObjects = [1, '25', {}, []];
+
+  test.each(variousObjects)('throws if createKey is not a function', (createKeyFunction) => {
+    expect(() => new HashSet(createKeyFunction)).toThrow('createKeyFunction is not a function');
+  });
+
+  test.each(variousObjects)('createKey method is default when no function is passed', (item) => {
+    const set = new HashSet();
+    const key = set.createKey_(item);
+    expect(key).toStrictEqual(item);
+  });
+
+  test('createKey method works as expected when set', () => {
+    const createKeyFunction = (item) => {
+      const {id, ...data} = item;
+      return data;
+    };
+    const set = new HashSet(createKeyFunction);
+    const data = {a: 25, b: [35, 45]};
+    const item = {id: 1234, ...data};
+    expect(set.createKey_(item)).toStrictEqual(data);
+  });
+
+});
+
+/**
+ * add tests
+ */
+describe('add', () => {
+
+  test.each([null, undefined])('throws if item is null or undefined', (item) => {
+    expect(() => new HashSet().add(item)).toThrow(`item is ${item}`);
+  });
+
+  test('adds item and returns key', () => {
+    const set = new HashSet();
+    const item = 25;
+    const key = set.add(item);
+    expect(set.size()).toBe(1);
+    expect(key).toBe(item);
+  });
+
+  test('same item twice only adds item once', () => {
+    const set = new HashSet();
+    const item = 25;
+    set.add(item);
+    set.add(item);
+    expect(set.size()).toBe(1);
+  });
+
+});
+
+/**
+ * get tests
+ */
+describe('get', () => {
+
+  test.each([null, undefined])('returns null if key is null or undefined', (key) => {
+    expect(new HashSet().get(key)).toBeNull();
+  });
+
+  test('returns null for unknown key', () => {
+    expect(new HashSet().get(12)).toBeNull();
+  });
+
+  test('returns value at key', () => {
+    const set = new HashSet();
+    const value = 55;
+    const key = set.add(55);
+    expect(set.get(key)).toBe(value);
+  });
+
+});
+
+/**
+ * remove tests
+ */
+describe('remove', () => {
+
+  test('does nothing if item does not exist', () => {
+    const set = new HashSet();
+    const item = 25;
+    set.add(item);
+    set.remove(item + 1);
+    expect(set.size()).toBe(1);
+    expect(set.get(item)).toBe(item);
+  });
+
+  test('removes item', () => {
+    const set = new HashSet();
+    const item = 25;
+    set.add(item);
+    set.remove(item);
+    expect(set.size()).toBe(0);
+    expect(set.get(item)).toBeNull();
+  });
+
+});
+
